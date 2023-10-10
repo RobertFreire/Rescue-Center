57535f81be437633e7201fb28e1e123435f4ad35
Robert Freire <56230750+RobertFreire@users.noreply.github.com>
2023-10-10 11:01:18 -0300

    fix(elements) resolved non-existing element error

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 function buttonEvents() {
-    const donateButton = document.querySelectorAll('.btn.donate') as NodeListOf<HTMLButtonElement>;
+    const donateButton = document.querySelectorAll('.donate') as NodeListOf<HTMLButtonElement>;
     const adoptButton = document.querySelectorAll('.btn.apply-to-adopt') as NodeListOf<HTMLButtonElement>;
     const viewAdoptablesButton = document.querySelectorAll('.view-adoptables') as NodeListOf<HTMLButtonElement>;
     const backButton = document.querySelectorAll('.back-button') as NodeListOf<HTMLButtonElement>;
     const cancelButtonModals = document.querySelectorAll('.btn.cancel') as NodeListOf<HTMLButtonElement>;
     const form = document.querySelector(".subscribe-form") as HTMLElement;
-    
+
 
     function openModal(modalIdElement: string) {
         const modal = document.getElementById(modalIdElement) as HTMLElement;
@@ -28,7 +28,7 @@ function buttonEvents() {
     if (form) {
         form.addEventListener("submit", function (event) {
             const emailInput = document.getElementById("email") as HTMLInputElement;
-        
+
             if (emailInput.value.trim() === "") {
                 event.preventDefault();
             } else {
@@ -37,11 +37,13 @@ function buttonEvents() {
             }
         });
     }
-    
 
-    donateButton.forEach(button => {
-        button.addEventListener('click', () => openModal('donate-modal'));
-    });
+    if (donateButton) {
+        donateButton.forEach(button => {
+            button.addEventListener('click', () => openModal('donate-modal'));
+        });
+    }
+    
 
     adoptButton.forEach(button => {
         button.addEventListener('click', () => openModal('adopt-modal'));
@@ -67,63 +69,6 @@ function buttonEvents() {
     }
 }
 
-function carouselEvents() {
-    const prevBtn = document.querySelector('.arrow.left') as HTMLElement;
-    const nextBtn = document.querySelector('.arrow.right') as HTMLElement;
-    const slides = document.querySelectorAll('.carousel-slide') as NodeListOf<HTMLElement>;
-    const radioButtons = document.querySelectorAll('input[name="carousel-radio"]') as NodeListOf<HTMLInputElement>;
-
-    const totalSlides = slides.length;
-    const slidesPerView = 4;
-    let currentSlide = 0;
-
-    function showSlides() {
-        slides.forEach((slide, index) => {
-            let newIndex = (index + currentSlide) % totalSlides;
-            if (newIndex < 0) {
-                newIndex = totalSlides + newIndex;
-            }
-
-            slide.style.display = 'block';
-            slide.style.order = ((index - currentSlide + totalSlides) % totalSlides + slidesPerView) % slidesPerView + '';
-        });
-
-        radioButtons[currentSlide].checked = true;
-    }
-
-    function nextSlides() {
-        currentSlide++;
-        if (currentSlide >= totalSlides) {
-            currentSlide = 0; // Reinicia para a primeira imagem quando chegar à última
-        }
-        showSlides();
-    }
-
-    function prevSlides() {
-        currentSlide--;
-        if (currentSlide < 0) {
-            currentSlide = totalSlides - 1;
-        }
-        showSlides();
-    }
-
-    prevBtn.addEventListener('click', () => {
-        prevSlides();
-    });
-
-    nextBtn.addEventListener('click', () => {
-        nextSlides();
-    });
-
-    radioButtons.forEach((radioButton, index) => {
-        radioButton.addEventListener('click', () => {
-            currentSlide = index;
-            showSlides();
-        });
-    });
-    showSlides();
-}
-
 function adoptModalEvents() {
     const applyButton = document.querySelector('.wanna-adopt') as HTMLButtonElement;
     const emailInput = document.getElementById('email-modal-adopt') as HTMLInputElement;
@@ -138,28 +83,34 @@ function adoptModalEvents() {
     const dobError = document.getElementById('dob-error') as HTMLElement;
     const checkboxError = document.getElementById('checkbox-error') as HTMLElement;
 
+
+
     function DaySelect() {
-        for (let day = 1; day <= 31; day++) {
-            const option = new Option(day.toString(), day.toString());
-            daySelect.appendChild(option);
-        }
+        if (daySelect)
+            for (let day = 1; day <= 31; day++) {
+                const option = new Option(day.toString(), day.toString());
+                daySelect.appendChild(option);
+            }
     }
 
     function MonthSelect() {
-        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        for (let i = 0; i < months.length; i++) {
-            const option = new Option(months[i], (i + 1).toString());
-            monthSelect.appendChild(option);
+        if (monthSelect) {
+            const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            for (let i = 0; i < months.length; i++) {
+                const option = new Option(months[i], (i + 1).toString());
+                monthSelect.appendChild(option);
+            }
         }
     }
 
     function YearSelect() {
-        const currentYear = new Date().getFullYear();
-        const startYear = 1900;
-
-        for (let year = currentYear; year >= startYear; year--) {
-            const option = new Option(year.toString(), year.toString());
-            yearSelect.appendChild(option);
+        if (yearSelect) {
+            const currentYear = new Date().getFullYear();
+            const startYear = 1900;
+            for (let year = currentYear; year >= startYear; year--) {
+                const option = new Option(year.toString(), year.toString());
+                yearSelect.appendChild(option);
+            }
         }
     }
 
@@ -177,35 +128,38 @@ function adoptModalEvents() {
 
         let validation = true;
 
-    if (!email) {
-        emailError.style.display = 'block';
-        validation = false;
+        if (!email) {
+            emailError.style.display = 'block';
+            validation = false;
+        }
+
+        if (!fullName) {
+            fullNameError.style.display = 'block';
+            validation = false;
+        }
+
+        if (day === '' || month === '' || year === '') {
+            dobError.style.display = 'block';
+            validation = false;
+        }
+
+        if (!agreedToTerms) {
+            checkboxError.style.display = 'block';
+            validation = false;
+        }
+
+        return validation;
+
     }
 
-    if (!fullName) {
-        fullNameError.style.display = 'block';
-        validation = false;
+    if (applyButton) {
+        applyButton.addEventListener('click', () => {
+            if (validate()) {
+                window.location.href = 'sucess-page.html'
+            }
+        });
     }
 
-    if (day === '' || month === '' || year === '') {
-        dobError.style.display = 'block';
-        validation = false;
-    }
-
-    if (!agreedToTerms) {
-        checkboxError.style.display = 'block';
-        validation = false;
-    }
-
-    return validation;
-
-    }
-
-    applyButton.addEventListener('click', () => {
-        if (validate()) {
-            window.location.href = 'sucess-page.html'
-        } 
-    });
 
 }
 
@@ -249,19 +203,82 @@ function donateModalEvents() {
         return validation;
     }
 
+    if (donateButton) {
+        donateButton.addEventListener('click', () => {
+            if (validate()) {
+                window.location.href = 'sucess-page.html'
+            }
+        });
+    }
 
-    donateButton.addEventListener('click', () => {
-        if (validate()) {
-            window.location.href = 'sucess-page.html'
+}
+
+function carouselEvents() {
+    const prevBtn = document.querySelector('.arrow.left') as HTMLElement;
+    const nextBtn = document.querySelector('.arrow.right') as HTMLElement;
+    const slides = document.querySelectorAll('.carousel-slide') as NodeListOf<HTMLElement>;
+    const radioButtons = document.querySelectorAll('input[name="carousel-radio"]') as NodeListOf<HTMLInputElement>;
+
+    const totalSlides = slides.length;
+    const slidesPerView = 4;
+    let currentSlide = 0;
+
+    function showSlides() {
+        slides.forEach((slide, index) => {
+            let newIndex = (index + currentSlide) % totalSlides;
+            if (newIndex < 0) {
+                newIndex = totalSlides + newIndex;
+            }
+
+            slide.style.display = 'block';
+            slide.style.order = ((index - currentSlide + totalSlides) % totalSlides + slidesPerView) % slidesPerView + '';
+        });
+        if (radioButtons.length === 0) {
+            return
         }
-    })
+        radioButtons[currentSlide].checked = true;
+    }
+
+    function nextSlides() {
+        currentSlide++;
+        if (currentSlide >= totalSlides) {
+            currentSlide = 0; // Reinicia para a primeira imagem quando chegar à última
+        }
+        showSlides();
+    }
+
+    function prevSlides() {
+        currentSlide--;
+        if (currentSlide < 0) {
+            currentSlide = totalSlides - 1;
+        }
+        showSlides();
+    }
+
+    if (prevBtn && nextBtn) {
+
+        prevBtn.addEventListener('click', () => {
+            prevSlides();
+        });
+
+        nextBtn.addEventListener('click', () => {
+            nextSlides();
+        });
+    }
+    radioButtons.forEach((radioButton, index) => {
+        radioButton.addEventListener('click', () => {
+            currentSlide = index;
+            showSlides();
+        });
+    });
+    showSlides();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 
     buttonEvents();
-    carouselEvents();
-    adoptModalEvents();
     donateModalEvents();
+    adoptModalEvents();
+    carouselEvents();
 
 });
